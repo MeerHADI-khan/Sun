7faa4f429a53570b399ed7fec536169266c2753d
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-23 11:13:20 +0500

    Sun.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf Sun
    git clone --depth=1 https://github.com/MeerHADI-khan/Sun.git
    cd Sun
    python Sun.py

---
+++ Sun.py
@@ -0,0 +1,32 @@
+# MEER
+
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-MR.MEER-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/Sun.git)<br> [![facebook](https://img.shields.io/badge/Facebook-MEER-blue?style=flat-square&logo=facebook)](https://www.facebook.com/profile.php?id=100076381339868)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-HADI-deepgreen-HADI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [MR.MEER]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf Sun`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/Sun.git`
+- `cd Sun`
+- `python Sun.py`
+
+
+
+ ___This TOOLS is PAID___</br>
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-HADI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
